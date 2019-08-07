1719f275218928f8c10986dfeff57e2725632be6
Sahay <mayank.2.sahay@bt.com>
2019-08-07 12:36:15 +0530

    ReactiveFormModule

--- EuroFinAngularApp/src/app/login/login.component.ts
+++ EuroFinAngularApp/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{ FormBuilder , FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+loginForm:FormGroup;
+  constructor(private fb :FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm= new FormGroup({
+      UserName:new FormControl(),
+      Password: new FormControl()
+    })
   }
+
+  signIn=function(){
+console.log(this.loginForm);
+  };
 
 }
