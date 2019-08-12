5169807e6dfddf18ea009c5082ca0c87483a75f8
Mayank Sahay <hellomayanksahay@Gmail.com>
2019-08-13 01:30:56 +0530

    register

--- EuroFinAngularApp/src/app/login.service.ts
+++ EuroFinAngularApp/src/app/login.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) { }
+
+  register(registerForm:any){
+    return this.httpClient.post<any>('http://localhost:59692/api/register',{"UserName":registerForm.UserName,
+  "EmailId": registerForm.EmailId,"PhoneNo":registerForm.Phone,"Password":registerForm.Password},{});
+  };
+
+  saveTask(note:any){
+    return this.httpClient.post<any>('http://localhost:59692/api/mytodolist',{"Note":note},{});
+  }
+
+  deleteTask(note:any){
+    return this.httpClient.delete<any>('http://localhost:59692/api/mytodolist'+'/'+note.Id);
+  }
+
 }
--- EuroFinAngularApp/src/app/register/register.component.ts
+++ EuroFinAngularApp/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+private registerForm:FormGroup;
+  constructor(private fb:FormBuilder,private loginService:LoginService) { }
 
   ngOnInit() {
+    this.registerForm= this.fb.group({
+      UserName:['',[Validators.required]],
+      EmailId:['', [Validators.required, Validators.email]],
+      Phone:['',[Validators.required]],
+      Password:['',[Validators.required]]
+    });
+  }
+
+  signUp(){
+
+    if(this.registerForm.invalid){
+      return;
+    }
+    // alert("Hi");
+
+    this.loginService.register(this.registerForm.value).subscribe(
+      (data) =>{
+        //this.getTask();
+    //this.addTask="";
+    
+      },
+      () =>{}
+    );
   }
 
 }
